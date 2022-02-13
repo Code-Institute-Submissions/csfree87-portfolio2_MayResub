3c33e7585d2ec1d5ce95b0fb79f04c53da47f2f8
csfree87 <csfree@icloud.com>
2022-02-13 01:20:49 +0000

    Add functions for questions and answers in quizgame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Quiz questions and answers array
+ */
 const quizData = [
     {
       question: "Who gave Harry the Gillyweed in book four?",
@@ -64,3 +67,49 @@ const quizData = [
     correct: "b",
 },
 ];
+
+/**
+ * Quiz loading and operating functions and scoring functions
+ */
+
+ const quiz = document.getElementById('quiz');
+ const answerEls = document.querySelectorAll('.answer');
+ const questionEl = document.getElementById('question');
+ const a_text = document.getElementById('a_text');
+ const b_text = document.getElementById('b_text');
+ const c_text = document.getElementById('c_text');
+ const d_text = document.getElementById('d_text');
+ const submitBtn = document.getElementById('submit');
+ 
+ let currentQuiz = 0;
+ let score = 0;
+ 
+ loadQuiz();
+ 
+ function loadQuiz() {
+     deselectAnswers();
+ 
+     const currentQuizData = quizData[currentQuiz];
+ 
+     questionEl.innerText = currentQuizData.question;
+     a_text.innerText = currentQuizData.a;
+     b_text.innerText = currentQuizData.b;
+     c_text.innerText = currentQuizData.c;
+     d_text.innerText = currentQuizData.d;
+ }
+ 
+ function deselectAnswers() {
+     answerEls.forEach(answerEl => answerEl.checked = false);
+ }
+ 
+ function getSelected() {
+     let answer;
+ 
+     answerEls.forEach(answerEl => {
+         if(answerEl.checked) {
+             answer = answerEl.id;
+         }
+     });
+ 
+     return answer;
+ }
